e5e080b0c755a9d5bb9f94346db0503afc580bdc
Tim Clifford <tclifford@protonmail.com>
2021-04-29 12:58:37 +0100

    Add some magic to fix links in wiki

--- wiki-export/update.sh
+++ wiki-export/update.sh
@@ -8,12 +8,18 @@ git pull --rebase
 # Combine all the pages in docs/
 # Replace links
 # Remove extra license notices
+# Convert to mediawiki
+# Prepend wiki-specific file
 
 markdown_tmpfile=$(mktemp)
 cat ../../README.md >> $markdown_tmpfile
 echo $markdown_tmpfile
 
-for f in $(find ../../docs -name '*.md'); do
+map=""
+for f in $(ls ../../docs | grep '\.md'); do
+	# get mapping of section names to file names
+	map="$map:$f|$(sed -n 's/^# *//p' ../../docs/$f | head -n1)"
+
 	echo >> $markdown_tmpfile
 	# Ripgrep for this one. I'm not writing a crazy perl/awk script for
 	# something so simple
@@ -26,13 +32,19 @@ See [DOC_LICENSE](../DOC_LICENSE).
 
 [cc-by-sa]: http://creativecommons.org/licenses/by-sa/4.0/
 [cc-by-sa-image]: https://licensebuttons.net/l/by-sa/4.0/88x31.png' \
-	$f >> $markdown_tmpfile
+	../../docs/$f >> $markdown_tmpfile
 done
 
 # Replace links to other sections
-sed -i 's|\[[^\]]*](docs/\([^)]*\))|[[Hades#\1]]|g' $markdown_tmpfile
 
-# Move License to the end
+map="$(echo $map | tr ':' '\n')"
+IFS="
+"
+for i in $map; do
+	# This is the most disgusting thing I have ever written
+	sed -i "$(echo "$i" | awk -F"|" \
+		'{print "s|\\[[^]]*](docs/"$1")|[[Hades#"$2"]]|g"}')" $markdown_tmpfile
+done
 
 # Replace links to github
 # too big brain for sed
@@ -40,7 +52,9 @@ perl -i -pe \
 	's|]\((?!http)([^)]*)\)|](https://github.com/smh-my-head/hades/$1)|g' \
 	$markdown_tmpfile
 
-pandoc --from markdown --to mediawiki $markdown_tmpfile -o Hades.mw
+
+cp ../wiki.mw Hades.mw
+pandoc --from markdown --to mediawiki $markdown_tmpfile >> Hades.mw
 
 git add .
 git commit -m "Update Hades from latest on GitHub"
